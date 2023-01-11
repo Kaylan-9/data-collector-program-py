62544989b96375885f6d853603b65213ff2a3d9b
Kaylan-9 <66222520+Kaylan-9@users.noreply.github.com>
2023-01-11 20:19:28 -0300

    :sparkles: feat: get relationship status

--- init.py
+++ init.py
@@ -4,13 +4,21 @@ from playwright.sync_api import sync_playwright
 
 load_dotenv(find_dotenv())
 
+def collect(page):
+  page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span').click()
+  page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div[1]/div/a').click()
+  page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[3]/div/div/div/div[1]/div/div/div[1]/div/div/div/div/div/div/a[2]').click()
+  text = page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[6]/div/div/div[1]/div/div[2]/div/span').text_content()
+  print(text)
+  page.wait_for_timeout(9000)
+
+
 def login(page):
   useremail = os.environ.get('EMA')
   password = os.environ.get('PAS')
   page.locator('xpath=//*[@id="email"]').fill(useremail)
   page.locator('xpath=//*[@id="pass"]').fill(password)
   page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button').click()
-  page.wait_for_timeout(9000)
 
 def init(p):
   url = os.environ.get('URL')
@@ -18,7 +26,7 @@ def init(p):
   page = browser.new_page()
   page.goto(url)
   login(page)
-  page.screenshot(path="screenshot.png", full_page=True)
+  collect(page)
   browser.close()
 
 with sync_playwright() as p:
