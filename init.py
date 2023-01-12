c4602ade6ce9628b1b572b299218c6347b06d611
Kaylan-9 <66222520+Kaylan-9@users.noreply.github.com>
2023-01-11 22:05:26 -0300

    :ok_hand: refactor: adjusting the format of worksheets

--- init.py
+++ init.py
@@ -9,9 +9,14 @@ def collect(page):
   page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span').click()
   page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div[1]/div/a').click()
   page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[3]/div/div/div/div[1]/div/div/div[1]/div/div/div/div/div/div/a[2]').click()
-  text = page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[6]/div/div/div[1]/div/div[2]/div/span').text_content()
-  page.wait_for_timeout(9000)
-  return text
+  relationship_status = page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[6]/div/div/div[1]/div/div[2]/div/span').text_content()
+  page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[1]/div[5]/a').click()
+  sexual_gender = page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[3]/div[2]/div/div/div[1]/div/div[2]/div/div[1]/div/div/div[1]/span').text_content()
+  page.wait_for_timeout(1000)
+  return {
+    "relationship_status": relationship_status,
+    "sexual_gender": sexual_gender
+  }
 
 def login(page):
   useremail = os.environ.get('EMA')
@@ -23,7 +28,10 @@ def login(page):
 def create_spreadsheet(data):
   wb = Workbook()
   ws = wb.active
-  ws['A1'] = data
+  ws['A1'] = 'status de relacionamento'
+  ws['A2'] = data.get("relationship_status")
+  ws['B1'] = 'gênero sexual'
+  ws['B2'] = data.get("sexual_gender")
   wb.save("data.xlsx")
 
 def init(p):
