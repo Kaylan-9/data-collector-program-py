2d2b5e997356bc4ce235f118199423e93677b22a
Kaylan-9 <66222520+Kaylan-9@users.noreply.github.com>
2023-01-11 23:11:38 -0300

    :recycle: refactor: creating data collection class

--- init.py
+++ init.py
@@ -1,22 +1,29 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 from playwright.sync_api import sync_playwright
+from openpyxl.styles import PatternFill
 from openpyxl import Workbook
 
 load_dotenv(find_dotenv())
 
-def collect(page):
-  page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span').click()
-  page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div[1]/div/a').click()
-  page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[3]/div/div/div/div[1]/div/div/div[1]/div/div/div/div/div/div/a[2]').click()
-  relationship_status = page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[6]/div/div/div[1]/div/div[2]/div/span').text_content()
-  page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[1]/div[5]/a').click()
-  sexual_gender = page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[3]/div[2]/div/div/div[1]/div/div[2]/div/div[1]/div/div/div[1]/span').text_content()
-  page.wait_for_timeout(1000)
-  return {
-    "relationship_status": relationship_status,
-    "sexual_gender": sexual_gender
-  }
+class Collect:
+  def __init__(self, page):
+    self.page = page
+    self.data = {}
+    self.page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[1]/span').click()
+    self.page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div[1]/div/a').click()
+    self.page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[3]/div/div/div/div[1]/div/div/div[1]/div/div/div/div/div/div/a[2]').click()
+  def set_relationship_status(self):
+    self.data["A1"] = 'status de relacionamento'
+    self.data["A2"] = self.page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[6]/div/div/div[1]/div/div[2]/div/span').text_content()
+  def set_sexual_gender(self):
+    self.page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[1]/div[5]/a').click()
+    self.data["B1"] = 'gênero sexual'
+    self.data["B2"] = self.page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[3]/div[2]/div/div/div[1]/div/div[2]/div/div[1]/div/div/div[1]/span').text_content()
+  def set_date_birthday(self):
+    self.data["C1"] = 'data de aniversário'
+    self.data["C2"] = self.page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[3]/div[3]/div/div/div[1]/div/div[2]/div[1]/div[1]/div/div/div[1]/span').text_content()
+    
 
 def login(page):
   useremail = os.environ.get('EMA')
@@ -28,10 +35,10 @@ def login(page):
 def create_spreadsheet(data):
   wb = Workbook()
   ws = wb.active
-  ws['A1'] = 'status de relacionamento'
-  ws['A2'] = data.get("relationship_status")
-  ws['B1'] = 'gênero sexual'
-  ws['B2'] = data.get("sexual_gender")
+  for key, value in data.items():
+    ws[key] = value
+    if key.find("1")>-1:
+      ws[key].fill = PatternFill(bgColor="FFC7CE", fill_type = "solid")
   wb.save("data.xlsx")
 
 def init(p):
@@ -40,8 +47,13 @@ def init(p):
   page = browser.new_page()
   page.goto(url)
   login(page)
-  data = collect(page)
-  create_spreadsheet(data)
+
+  collect = Collect(page)
+  collect.set_relationship_status()
+  collect.set_sexual_gender()
+  collect.set_date_birthday()
+  create_spreadsheet(collect.data)
+
   browser.close()
 
 with sync_playwright() as p:
@@ -49,4 +61,7 @@ with sync_playwright() as p:
     init(p)
   except ValueError:
     print(ValueError)
+
+
+
     
