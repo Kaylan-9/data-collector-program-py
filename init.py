057cfc286b3364d217d532ba4b57306358e95cf1
Kaylan-9 <66222520+Kaylan-9@users.noreply.github.com>
2023-01-11 21:44:00 -0300

    :sparkles: feat: adding values to worksheet

--- init.py
+++ init.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 from playwright.sync_api import sync_playwright
+from openpyxl import Workbook
 
 load_dotenv(find_dotenv())
 
@@ -9,9 +10,8 @@ def collect(page):
   page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[2]/div[4]/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div/div[1]/div[1]/div/a').click()
   page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[3]/div/div/div/div[1]/div/div/div[1]/div/div/div/div/div/div/a[2]').click()
   text = page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div[1]/div/div/div/div/div[2]/div/div/div/div[6]/div/div/div[1]/div/div[2]/div/span').text_content()
-  print(text)
   page.wait_for_timeout(9000)
-
+  return text
 
 def login(page):
   useremail = os.environ.get('EMA')
@@ -20,13 +20,20 @@ def login(page):
   page.locator('xpath=//*[@id="pass"]').fill(password)
   page.locator('xpath=/html/body/div[1]/div[1]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button').click()
 
+def create_spreadsheet(data):
+  wb = Workbook()
+  ws = wb.active
+  ws['A1'] = data
+  wb.save("data.xlsx")
+
 def init(p):
   url = os.environ.get('URL')
   browser = p.chromium.launch()
   page = browser.new_page()
   page.goto(url)
   login(page)
-  collect(page)
+  data = collect(page)
+  create_spreadsheet(data)
   browser.close()
 
 with sync_playwright() as p:
